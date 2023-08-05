e870dc5fd8e9d27d79e45dd66edc9b8dbafdd295
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 03:10:33 +0300

    add models.py

--- todos_func_app/models.py
+++ todos_func_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tasks(models.Model):
+    Title = models.CharField(max_length=150)
+    # Auto_now_add adds time on creation action
+    created_at = models.DateTimeField(auto_now_add=True)
+    # While auto_now adds time on save action
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.Title
