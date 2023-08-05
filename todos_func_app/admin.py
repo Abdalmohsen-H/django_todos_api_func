a27233fa399f5fea03b1cf20d5c94f1d418a4bcb
Abdalmohsen-H <egywebdes@gmail.com>
2023-08-05 03:37:52 +0300

    create django super user and register model to app's admin.py file

--- todos_func_app/admin.py
+++ todos_func_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here to be able to manage it from django admin dashboard.
+from .models import Tasks
+
+admin.site.register(Tasks)
